61d7563326f1478be617a6d0e9da5e7375f3cf31
mallin <malin.lindvall@sinch.com>
2023-06-10 11:23:42 +0200

    day 3 part 2 solution

--- AdvC_03.py
+++ AdvC_03.py
@@ -13,6 +13,19 @@ for line in list_from_rugsack:
     common_words = ''.join(set(first_half).intersection(second_half))
     my_score += character_value_dict[common_words]
 
-print(my_score)
+print(f'My first score: {my_score}')
 
+
+"""Day 3 Advent of Code 2022 - Assignment 2"""
+my_second_score = 0
+
+    
+for item in range(0,len(list_from_rugsack)-2, 3):
+    set1 = set(list_from_rugsack[item])
+    set2 = set(list_from_rugsack[item + 1])
+    set3 = set(list_from_rugsack[item + 2])
+    common_char = ', '.join(list(map(str, set1.intersection(set2, set3))))
+    my_second_score += character_value_dict[common_char]
+
+print(f'My second score: {my_second_score}')
 
