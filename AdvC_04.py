d570aa177a3cc70bcf9c7f7c9e8a1a2be54d3bfd
mallin <malin.lindvall@sinch.com>
2023-06-12 20:07:01 +0200

    day 4 part 2 solution

--- AdvC_04.py
+++ AdvC_04.py
@@ -17,3 +17,20 @@ for item in input_file:
 
 print(count_contains_subset)
 
+"""Day 4 Advent of Code 2022 - Assignment 2"""
+
+count_contains_same_value = 0
+
+for item in input_file:
+    first_part, second_part = item.split(',')
+    start_value_first_part, stop_value_first_part = first_part.split('-')
+    start_value_second_part, stop_value_second_part = second_part.split('-')
+    range_values_first_part = set(range(int(start_value_first_part), int(stop_value_first_part) +1))
+    range_values_second_part = set([value for value in range(int(start_value_second_part), int(stop_value_second_part)+1)])
+
+    if range_values_first_part.intersection(range_values_second_part):
+        count_contains_same_value += 1
+    
+
+print(count_contains_same_value)
+
