1d6ae239ef6a26a37acef4d4533c9d08a56d5532
mallin <malin.lindvall@sinch.com>
2023-06-12 11:53:51 +0200

    day 4 part 1 solution

---
+++ AdvC_04.py
@@ -0,0 +1,19 @@
+"""Day 4 Advent of Code 2022 - Assignment 1"""
+
+input_file = open('input_04.txt', 'r').readlines()
+count_contains_subset = 0
+
+for item in input_file:
+    first_part, second_part = item.split(',')
+    start_value_first_part, stop_value_first_part = first_part.split('-')
+    start_value_second_part, stop_value_second_part = second_part.split('-')
+    range_values_first_part = set(range(int(start_value_first_part), int(stop_value_first_part) +1))
+    range_values_second_part = set([value for value in range(int(start_value_second_part), int(stop_value_second_part)+1)])
+    
+
+    if (range_values_first_part.issubset(range_values_second_part) or range_values_second_part.issubset(range_values_first_part))  :
+        count_contains_subset += 1
+
+
+print(count_contains_subset)
+
