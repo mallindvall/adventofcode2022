478a24690c976426819bfe2a61cad054752b079b
Malin Lindvall <malin.lindvall@solita.fi>
2023-07-04 15:40:49 +0200

    Day 6, solution part 1 & 2 reworked

--- AdvC_06.py
+++ AdvC_06.py
@@ -1,67 +1,21 @@
 
-#example input:  qwerrttjfkfld
-#take first 4 characters in string > see if they are unique
-# if yes take the index at the last character in the string (+1)
-# if not move one character ahead in the string and make the same comparison
 
-input_file = open('input_06.txt', 'r').readlines()
-
-counter = 0
-run = True
-run_2 = True
-stop_index = []
-stop_index_2 = []
-
-def has_multipte_characters(input_to_compare):
-    characters_count = {}
-    for char in input_to_compare:
-        if char in characters_count:
-            return True
-        characters_count[char] = 1
-    return False
+with open('input_06.txt', 'r') as file:
+    input_file = file.read()
 
 
 # ==== PART 1 ====
 
+for input in range(4, len(input_file)): #move from the 5th character to the end of the input
+    block = set(input_file[(input -4): input]) #take the charachters before the 5th character upuntil the 5th character. Set cannot contain duplicates. Will only displat unique characters.
+    if len(block) == 4:
+        print(f'Answer for part 1 is: {input}')
+        break
 
-for input in input_file:   
-    for element in input:
-        while run == True:
-            input_to_compare = input[counter:counter + 4]
-            counter += 1
-            if has_multipte_characters(input_to_compare) == False: 
-                run = False
-            stop_index.append(element)
-            
+# ==== PART 2 ====    
 
-print(len(stop_index)+3)
-
-     
-# ==== PART 2 ====
-        
-for input in input_file:   
-    for element in input:
-        while run_2 == True:
-            input_to_compare_message = input[counter:counter + 14]
-            counter += 1
-            if has_multipte_characters(input_to_compare_message) == False: 
-                run_2 = False
-            stop_index_2.append(element)
-            
-print(len(stop_index_2)+ 13)       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-'''
-if input_to_compare.count(element) > 1:
-    print('yes', input_to_compare, element) 
-else: print('no', input_to_compare, element)
-counter += 1
- '''
-        
+for input in range(14, len(input_file)): 
+    block = set(input_file[(input -14): input])
+    if len(block) == 14:
+        print(f'Answer for part 2 is: {input}')
+        break
